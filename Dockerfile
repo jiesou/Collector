62f1ada05620d318e274bb75c43e09e11b7699f1
jiesou <jiechushou@gmail.com>
2023-02-22 19:03:55 +0800

    使用 docker volumes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM registry.baidubce.com/paddlepaddle/paddle:2.4.1
 
-WORKDIR /app
+WORKDIR /root
 
 COPY app/requirements.txt .
-RUN python -m pip install -r requirements.txt
+RUN python3 -m pip install -r requirements.txt
 
 COPY app .
--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -1,4 +1,4 @@
-docker rm -f paddleocr
-docker build --tag paddleocr .
-docker run --name paddleocr -idt --mount 'type=volume,src=paddleocr,dst=/root/.paddleocr' paddleocr
-docker exec -it paddleocr python3 -m ocr
+docker rm -f collector
+docker build --tag collector .
+docker run --name collector -idt --volume ./app/:/root/:ro collector
+docker exec -it collector python3 -m ocr en
