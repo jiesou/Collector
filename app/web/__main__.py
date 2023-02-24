53b54c3b44c3582ad20c1b2a0c011241f4e39543
jiesou <jiechushou@gmail.com>
2023-02-24 22:15:58 +0800

    初步 webui

--- app/web/__main__.py
+++ app/web/__main__.py
@@ -1,24 +1,37 @@
-from flask import Flask
+from flask import Flask, send_from_directory, request
 from werkzeug.exceptions import HTTPException
-from units import response
+from .units import response
 
-app = Flask(__name__,
-    static_url_path='/ui', 
-    static_folder='ui')
-
-@app.route("/")
-def index():
-    return response({
-        "status": "running",
-        "version": "1.0.0"})
+app = Flask(__name__)
 
 @app.errorhandler(HTTPException)
 def http_error(e):
     res = e.get_response()
     res.data = response(app, {
-        "code": e.code,
-        "message": e.name}).data
-    res.content_type = "application/json"
+        'code': e.code,
+        'message': e.name}).data
+    res.content_type = 'application/json'
     return res
 
-app.run(port=3000)
+@app.route('/')
+def index():
+    return send_from_directory('ui', 'index.html')
+
+@app.route('/<path:file>')
+def serve_results(file):
+    return send_from_directory('ui', file)
+
+@app.route('/upload', methods=['POST'])
+def upload_imgs():
+    file = request.files['file']
+    if file.filename != '':
+        file.save(file.filename)
+    return redirect('/')
+
+@app.route('/api')
+def api_index():
+    return response(app, {
+        'status': 'running',
+        'version': '1.0.0'})
+
+app.run(port=3000, use_reloader=True)
--- app/web/units.py
+++ app/web/units.py
@@ -1,5 +1,3 @@
-import json
-from flask import Flask
 def response(app, mdict):
     #mdict["code"] = mdict.get("code", 0)
     mdict.setdefault("code", 0)
