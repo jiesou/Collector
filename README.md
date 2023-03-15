0d082f9bb3d1cdfc901bb60c4ea1c2b1dea66be5
jiesou <jiechushou@gmail.com>
2023-03-15 19:01:18 +0800

    完善 README

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Collector
-Homework Helper by AI
+Homework Helper by AI (WIP)
 
-WIP!!!
+# 运行
+
+需要支持 Docker 的环境
+
+```shell
+./scripts/reinstall.sh
+```
+
+脚本供参考，可根据需求调整命令
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,1 @@
-docker volume create paddleocr
-docker build --tag paddleocr .
-docker run --name paddleocr -idt --mount 'type=volume,src=paddleocr,dst=/root/.paddleocr' paddleocr
-docker exec -it paddleocr python3 -m ocr
+docker build --tag collector .
