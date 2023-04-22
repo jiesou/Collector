ffc2d7902a225482ab69cee80b31b7c1d767a2fb
jiesou <jiechushou@gmail.com>
2023-04-22 05:35:26 +0000

    docker 失败重启

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,3 +1,3 @@
 docker rm -f collector-dev
 docker build --tag collector-dev .
-docker run --name collector-dev -idt --volume ./app/:/root/ -p 3000:3000 --env-file ./.env collector-dev
+docker run --name collector-dev -idt --restart unless-stopped --volume ./app/:/root/ -p 3000:3000 --env-file ./.env collector-dev
--- scripts/reinstall.sh
+++ scripts/reinstall.sh
@@ -1,4 +1,4 @@
 docker rm -f collector
 docker build --tag collector .
-docker run --name collector -idt --volume ./app/:/root/ -p 80:3000 --env-file ./.env collector
+docker run --name collector -idt --restart unless-stopped --volume ./app/:/root/ -p 80:3000 --env-file ./.env collector
 #docker exec -it collector python3 -m ocr en
